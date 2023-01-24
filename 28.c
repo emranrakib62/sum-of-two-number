1539c453053999b2d5a4084dd7c39df03cd64be0
emranrakib62 <122172406+emranrakib62@users.noreply.github.com>
2023-01-24 21:38:27 +0600

    Add files via upload

---
+++ 28.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    int x,y,sum;
+    printf("enter the two number:");
+    scanf("%d%d",&x,&y);
+    sum=(x+y);
+    printf("the sum of two number are:%d",sum);
+return 0;
+}
